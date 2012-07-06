339f84dbba11faf7eef6c34404bbf53bfd4f3d79
Thomas Leveque <leveque.thomas@gmail.com>
2012-07-06 14:50:00 +0200

    fix build scripts

--- build.bat
+++ build.bat
@@ -1,13 +1,5 @@
-@echo --- --------------------------------------------------------------- ---
-@echo --- Install akquinet OSGi Deployment admin implementation and tools ---
-@echo --- --------------------------------------------------------------- ---
-@set MAVEN_OPTS=-Xms512m -Xmx1024m -XX:PermSize=256m -XX:MaxPermSize=512m
-@cd dependencies
-@cd osgi-deployment-admin
-@cmd /C mvn clean install
-@cd ..
-@cd ..
 @echo --- --------------------------------------------------------------- --- 
 @echo ---                       Build And Install ICasa                    ---
 @echo --- --------------------------------------------------------------- ---
+@set MAVEN_OPTS=-Xms512m -Xmx1024m -XX:PermSize=256m -XX:MaxPermSize=512m
 @cmd /C mvn clean install
