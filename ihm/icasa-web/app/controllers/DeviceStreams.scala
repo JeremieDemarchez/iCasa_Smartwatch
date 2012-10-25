cd03f5d2426c120074a6d7c4ebf5181eef1e81dd
Thomas Leveque <leveque.thomas@gmail.com>
2012-10-25 16:33:29 +0200

    add gogo command adpater for icasa scripts

---
+++ ihm/icasa-web/app/controllers/DeviceStreams.scala
@@ -0,0 +1,12 @@
+package controllers
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: thomas
+ * Date: 19/10/12
+ * Time: 13:50
+ * To change this template use File | Settings | File Templates.
+ */
+class DeviceStreams {
+
+}
