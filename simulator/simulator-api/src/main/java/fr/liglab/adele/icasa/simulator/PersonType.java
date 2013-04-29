5485a1642cdf564e64ae0702b83f627baa6e9bd2
torito <torito@gmail.com>
2013-04-29 18:21:16 +0200

    add PersonType class

---
+++ simulator/simulator-api/src/main/java/fr/liglab/adele/icasa/simulator/PersonType.java
@@ -0,0 +1,54 @@
+/**
+ *
+ *   Copyright 2011-2012 Universite Joseph Fourier, LIG, ADELE team
+ *   Licensed under a specific end user license agreement;
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *     http://adeleresearchgroup.github.com/iCasa-Simulator/snapshot/license.html
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+package fr.liglab.adele.icasa.simulator;
+
+/**
+ * User: torito
+ * Date: 4/29/13
+ * Time: 4:48 PM
+ */
+public class PersonType {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public PersonType(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        PersonType that = (PersonType) o;
+
+        if (!name.equals(that.name)) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return name.hashCode();
+    }
+
+
+
+}
