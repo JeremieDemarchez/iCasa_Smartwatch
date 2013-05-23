0924e74d66b4437716b4d268c8a593a8403f009a
Thomas Leveque <leveque.thomas@gmail.com>
2013-05-23 23:55:35 +0200

    fix #6 : Provide a skeleton of iCasa maven project

---
+++ app-skeletons/app-skeleton/application/src/main/java/myapp/EmptyApplication.java
@@ -0,0 +1,37 @@
+/**
+ *
+ *   Copyright 2011-2012 Universite Joseph Fourier, LIG, ADELE team
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+package myapp;
+
+
+import fr.liglab.adele.icasa.device.util.AbstractDeviceListener;
+import fr.liglab.adele.icasa.device.GenericDevice;
+import org.apache.felix.ipojo.annotations.*;
+
+
+@Component(name="empty-app")
+@Instantiate(name="empty-app-1")
+public class EmptyApplication {
+
+    @Validate
+    public void start() {
+        //TODO implement it
+    }
+
+    @Invalidate
+    public void stop() {
+        //TODO implement it
+    }
+}
