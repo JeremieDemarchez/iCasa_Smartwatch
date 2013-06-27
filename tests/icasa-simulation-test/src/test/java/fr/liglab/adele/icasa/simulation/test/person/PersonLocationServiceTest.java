e39765231820ae60063fd9bc5774f9943656fc8c
torito <torito@gmail.com>
2013-06-27 14:13:28 +0200

    change to ADELE 1.1.0

--- tests/icasa-simulation-test/src/test/java/fr/liglab/adele/icasa/simulation/test/person/PersonLocationServiceTest.java
+++ tests/icasa-simulation-test/src/test/java/fr/liglab/adele/icasa/simulation/test/person/PersonLocationServiceTest.java
@@ -1,3 +1,18 @@
+/**
+ *
+ *   Copyright 2011-2012 Universite Joseph Fourier, LIG, ADELE team
+ *   Licensed under a specific end user license agreement;
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *     http://adeleresearchgroup.github.com/iCasa-Simulator/snapshot/license.html
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
 package fr.liglab.adele.icasa.simulation.test.person;
 
 import java.util.Set;
