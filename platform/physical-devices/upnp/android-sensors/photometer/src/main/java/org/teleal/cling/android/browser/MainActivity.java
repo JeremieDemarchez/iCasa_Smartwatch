848e2569d95b96aaab050befe48a39eb11d2a036
torito <torito@gmail.com>
2013-07-04 10:48:11 +0200

    update license header file

--- platform/physical-devices/upnp/android-sensors/photometer/src/main/java/org/teleal/cling/android/browser/MainActivity.java
+++ platform/physical-devices/upnp/android-sensors/photometer/src/main/java/org/teleal/cling/android/browser/MainActivity.java
@@ -1,7 +1,7 @@
 /**
  *
- *   Copyright 2011-2012 Universite Joseph Fourier, LIG, ADELE team
- *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   Copyright 2011-2012 Universite Joseph Fourier, LIG, ADELE Research
+ *   Group Licensed under the Apache License, Version 2.0 (the "License");
  *   you may not use this file except in compliance with the License.
  *   You may obtain a copy of the License at
  *
@@ -13,7 +13,6 @@
  *   See the License for the specific language governing permissions and
  *   limitations under the License.
  */
-
 package main.java.org.teleal.cling.android.browser;
 
 import java.util.logging.Level;
