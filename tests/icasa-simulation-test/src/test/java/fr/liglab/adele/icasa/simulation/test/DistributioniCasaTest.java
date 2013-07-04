30dbefb01e95358848f9a8deaf754bc5c714ce97
torito <torito@gmail.com>
2013-07-04 11:08:55 +0200

    change ADELE team to ADELE Research Group

--- tests/icasa-simulation-test/src/test/java/fr/liglab/adele/icasa/simulation/test/DistributioniCasaTest.java
+++ tests/icasa-simulation-test/src/test/java/fr/liglab/adele/icasa/simulation/test/DistributioniCasaTest.java
@@ -1,7 +1,7 @@
 /**
  *
- *   Copyright 2011-2012 Universite Joseph Fourier, LIG, ADELE team
- *   Licensed under a specific end user license agreement;
+ *   Copyright 2011-2012 Universite Joseph Fourier, LIG, ADELE Research
+ *   Group Licensed under a specific end user license agreement;
  *   you may not use this file except in compliance with the License.
  *   You may obtain a copy of the License at
  *
@@ -13,7 +13,6 @@
  *   See the License for the specific language governing permissions and
  *   limitations under the License.
  */
-
 package fr.liglab.adele.icasa.simulation.test;
 
 import static org.junit.Assert.fail;
