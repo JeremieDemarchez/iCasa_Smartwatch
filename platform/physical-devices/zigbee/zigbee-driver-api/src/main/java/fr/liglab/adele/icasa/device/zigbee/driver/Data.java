0d6bc5f01831883f850c1bb597f32e83224f646b
Thomas Leveque <leveque.thomas@gmail.com>
2013-07-23 17:31:48 +0200

    add a custom zigbee driver api

---
+++ platform/physical-devices/zigbee/zigbee-driver-api/src/main/java/fr/liglab/adele/icasa/device/zigbee/driver/Data.java
@@ -0,0 +1,40 @@
+/**
+ *
+ *   Copyright 2011-2012 Universite Joseph Fourier, LIG, ADELE Research
+ *   Group Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+package fr.liglab.adele.icasa.device.zigbee.driver;
+
+import java.util.Date;
+
+/**
+ * Represents a data of a device.
+ *
+ * @author Thomas Leveque
+ */
+public interface Data {
+
+    /**
+     * Returns timestamp of data.
+     *
+     * @return timestamp of data.
+     */
+	Date getTimeStamp();
+
+    /**
+     * Returns data in a string format.
+     *
+     * @return data in a string format.
+     */
+	String getData();
+}
