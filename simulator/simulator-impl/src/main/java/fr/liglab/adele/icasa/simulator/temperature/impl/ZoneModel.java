7db880dffe4c00340df6f34af9f9b26870986172
Thomas Leveque <leveque.thomas@gmail.com>
2013-08-09 18:01:28 +0200

    take into account heater and cooler in temperature physical model

--- simulator/simulator-impl/src/main/java/fr/liglab/adele/icasa/simulator/temperature/impl/ZoneModel.java
+++ simulator/simulator-impl/src/main/java/fr/liglab/adele/icasa/simulator/temperature/impl/ZoneModel.java
@@ -1,9 +1,9 @@
 package fr.liglab.adele.icasa.simulator.temperature.impl;
 
+import fr.liglab.adele.icasa.location.LocatedDevice;
 import fr.liglab.adele.icasa.location.Zone;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Model of a zone used to compute thermal properties.
@@ -14,11 +14,20 @@ public class ZoneModel {
 
     private double _thermalCapacity;
     private Map<String, Double> _wallSurfaceMap = new HashMap<String, Double>();
-    private int _totalPower;
+    private double _totalPower;
     private Zone _zone;
+    private List<LocatedDevice> _devices = new ArrayList<LocatedDevice>();
 
     public ZoneModel(Zone zone) {
         this._zone = zone;
+    }
+
+    public Zone getZone() {
+        return _zone;
+    }
+
+    public String getZoneId() {
+        return _zone.getId();
     }
 
     public double getThermalCapacity() {
@@ -29,7 +38,7 @@ public class ZoneModel {
         this._thermalCapacity = thermalCapacity;
     }
 
-    public int getTotalPower() {
+    public double getTotalPower() {
         return _totalPower;
     }
 
@@ -37,12 +46,16 @@ public class ZoneModel {
         this._totalPower = totalPower;
     }
 
-    public void addPower(int powerToAdd) {
+    public void addPower(double powerToAdd) {
         this._totalPower += powerToAdd;
     }
 
-    public void reducePower(int powerToRemove) {
+    public void reducePower(double powerToRemove) {
         this._totalPower -= powerToRemove;
+    }
+
+    public Set<Map.Entry<String, Double>> getWallSurfaces() {
+        return _wallSurfaceMap.entrySet();
     }
 
     public double getWallSurface(String zoneId) {
@@ -59,5 +72,22 @@ public class ZoneModel {
 
     public void removeWallSurface(String zoneId) {
         _wallSurfaceMap.remove(zoneId);
+    }
+
+    public void addDevice(LocatedDevice device) {
+        _devices.add(device);
+    }
+
+    public void removeDevice(LocatedDevice device) {
+        _devices.remove(device);
+    }
+
+    /**
+     * Returns a mutable list of all devices into this zone.
+     *
+     * @return all devices into this zone.
+     */
+    public List<LocatedDevice> getDevices() {
+        return new ArrayList(_devices);
     }
 }
