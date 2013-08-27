8e5bcabe43c98dfe0d2a4f6751599730d6d07f0e
torito <torito@gmail.com>
2013-08-27 16:24:37 +0200

    add licence header in zigbee java files

--- platform/physical-devices/zigbee/zigbee-driver-impl/src/main/java/fr/liglab/adele/icasa/device/zigbee/driver/impl/ZigbeeDriverImpl.java
+++ platform/physical-devices/zigbee/zigbee-driver-impl/src/main/java/fr/liglab/adele/icasa/device/zigbee/driver/impl/ZigbeeDriverImpl.java
@@ -209,9 +209,9 @@ public class ZigbeeDriverImpl implements ZigbeeDriver {
 		try {
 			logger.debug("sending request response to device : " + moduleAddress + " with value : " + dataToSet);
 			handler.write(ResponseType.REQUEST, moduleAddress, dataToSet);
-			handler.write(buildResponseWithNewValue(ResponseType.REQUEST,
-					moduleAddress, dataValue.getData()
-								.equals("1") ? "0" : "1"));
+//			handler.write(buildResponseWithNewValue(ResponseType.REQUEST,
+//					moduleAddress, dataValue.getData()
+//								.equals("1") ? "0" : "1"));
 			
 		} catch (IOException e) {
 			logger.error("IOException caught while sending data to device : " + moduleAddress , e);
