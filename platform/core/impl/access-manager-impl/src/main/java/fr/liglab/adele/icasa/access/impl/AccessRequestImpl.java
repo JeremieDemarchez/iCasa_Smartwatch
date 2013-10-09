302f93ee29abec7bc3ccd6a98405749439b557b0
torito <torito@gmail.com>
2013-10-09 11:17:51 +0200

    update license file, and merge iCasaPlatform/platform into new repo structure

--- platform/core/impl/access-manager-impl/src/main/java/fr/liglab/adele/icasa/access/impl/AccessRequestImpl.java
+++ platform/core/impl/access-manager-impl/src/main/java/fr/liglab/adele/icasa/access/impl/AccessRequestImpl.java
@@ -1,18 +1,18 @@
-/*
- * Copyright 2011-2012 Universite Joseph Fourier, LIG, ADELE Research
- * Group Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+/**
  *
- * 	http://www.apache.org/licenses/LICENSE-2.0
+ *   Copyright 2011-2013 Universite Joseph Fourier, LIG, ADELE Research
+ *   Group Licensed under a specific end user license agreement;
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ *     http://adeleresearchgroup.github.com/iCasa/snapshot/license.html
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
  */
-
 package fr.liglab.adele.icasa.access.impl;
 
 import fr.liglab.adele.icasa.access.AccessRight;
