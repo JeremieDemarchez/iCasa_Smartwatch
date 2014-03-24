631ba70035e3c4859d8a9937970efb7a57954d76
aygalinc <colin.aygalinc@gmail.com>
2014-03-24 09:34:48 +0100

    Color Light

--- platform/core/api/context-api/src/main/java/fr/liglab/adele/icasa/device/light/ColorLight.java
+++ platform/core/api/context-api/src/main/java/fr/liglab/adele/icasa/device/light/ColorLight.java
@@ -1,3 +1,18 @@
+/**
+ *
+ *   Copyright 2011-2013 Universite Joseph Fourier, LIG, ADELE Research
+ *   Group Licensed under a specific end user license agreement;
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *     http://adeleresearchgroup.github.com/iCasa/snapshot/license.html
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
 package fr.liglab.adele.icasa.device.light;
 
 import java.awt.*;
@@ -8,14 +23,11 @@ import java.awt.*;
  */
 public interface ColorLight extends BinaryLight,DimmerLight{
 
-    String COLOR_LIGHT_COLOR_LEVEL = "colorLight.colorLevel";
-
-
     /**
      * Changes the colorlight
      * @param color
      */
-    public Color setColor(Color color);
+    public void setColor(Color color);
 
     /**
      * Fetches last color state
