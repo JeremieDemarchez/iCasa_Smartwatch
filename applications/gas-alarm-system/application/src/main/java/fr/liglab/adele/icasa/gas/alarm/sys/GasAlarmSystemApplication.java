e9ea84c8b913c8aad5d3035857afb962ad32e350
aygalinc <colin.aygalinc@gmail.com>
2014-04-02 17:45:52 +0200

    Re do gas Application

--- applications/gas-alarm-system/application/src/main/java/fr/liglab/adele/icasa/gas/alarm/sys/GasAlarmSystemApplication.java
+++ applications/gas-alarm-system/application/src/main/java/fr/liglab/adele/icasa/gas/alarm/sys/GasAlarmSystemApplication.java
@@ -1,193 +1,320 @@
 package fr.liglab.adele.icasa.gas.alarm.sys;
 
+
+import fr.liglab.adele.icasa.dependency.handler.annotations.RequiresDevice;
+import fr.liglab.adele.icasa.device.DeviceListener;
 import fr.liglab.adele.icasa.device.GenericDevice;
 import fr.liglab.adele.icasa.device.gasSensor.CarbonDioxydeSensor;
 import fr.liglab.adele.icasa.device.light.BinaryLight;
-import fr.liglab.adele.icasa.device.util.EmptyDeviceListener;
+import fr.liglab.adele.icasa.device.light.DimmerLight;
+import fr.liglab.adele.icasa.service.scheduler.PeriodicRunnable;
+import org.apache.felix.ipojo.annotations.*;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
 
-public class GasAlarmSystemApplication extends EmptyDeviceListener {
 
-	/**
-	 * @author jeremy
-	 * 
-	 */
-	private class AlarmThread extends Thread {
-
-		private boolean stopped = false;
-
-		public AlarmThread(String threadName) {
-			super(threadName);
-		}
-
-		public void stopAlarm() {
-			stopped = true;
-		}
-
-		@Override
-		public void run() {
-
-			while (!stopped) {
-
-				if (co2LevelTooHigh == true) {
-
-					List<BinaryLight> lightsToPilot = new ArrayList<BinaryLight>();
-					synchronized (binaryLights) {
-						for (BinaryLight binaryLight : binaryLights)
-							lightsToPilot.add(binaryLight);
-					}
-
-					for (BinaryLight binaryLight : lightsToPilot) {
-						binaryLight.setPowerStatus(false);
-					}
-
-					try {
-						Thread.sleep(1000);
-					} catch (InterruptedException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-
-                    for (BinaryLight binaryLight : lightsToPilot) {
-                        binaryLight.setPowerStatus(true);
-                    }
-
-                    try {
-                        Thread.sleep(1000);
-                    } catch (InterruptedException e) {
-                        // TODO Auto-generated catch block
-                        e.printStackTrace();
-                    }
+@Component(name = "GasAlarmSystemApplication")
+@Instantiate
+@Provides(specifications = PeriodicRunnable.class)
+public class GasAlarmSystemApplication implements DeviceListener,PeriodicRunnable {
 
 
-				}
+    public static final double SEUIL_CO2_CRITIC = 3.8;
 
-				else {
+    private boolean state = false;
 
-					try {
-						Thread.sleep(1000);
-					} catch (InterruptedException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-				}
-			}
-		}
-	}
+    private boolean alarmRunning = false;
 
-	public static double SEUIL_CO2_CRITIC = 3.8;
+    private final Object m_lock;
 
-	/** Field for binaryLight dependency */
-	private BinaryLight[] binaryLights;
 
-	/** Field for carbonDioxydeSensor dependency */
-	private CarbonDioxydeSensor[] carbonDioxydeSensors;
 
-	public Boolean co2LevelTooHigh = false;
+    @RequiresDevice(id="binaryLights", type="field", optional=true)
+    private BinaryLight[] binaryLights;
 
-	private AlarmThread alarmThread = null;
+    @RequiresDevice(id="dimmerLights", type="field", optional=true)
+    private DimmerLight[] dimmerLights;
 
-	/** Bind Method for null dependency */
-	public void bindBinaryLight(BinaryLight binaryLight, Map properties) {
-		//do nothing
-	}
+    @RequiresDevice(id="carbonDioxydeSensors", type="field", optional=true)
+    private CarbonDioxydeSensor[] carbonDioxydeSensors;
 
-	/** Unbind Method for null dependency */
-	public void unbindBinaryLight(BinaryLight binaryLight, Map properties) {
-        //do nothing
-	}
+    public GasAlarmSystemApplication() {
+        this.m_lock = new Object();
+    }
 
-	/** Bind Method for null dependency */
-	public void bindCarbonDioxydeSensor(
-			CarbonDioxydeSensor carbonDioxydeSensor, Map properties) {
-		carbonDioxydeSensor.addListener(this);
-	}
 
-	/** Unbind Method for null dependency */
-	public void unbindCarbonDioxydeSensor(
-			CarbonDioxydeSensor carbonDioxydeSensor, Map properties) {
-		carbonDioxydeSensor.removeListener(this);
-	}
-
-	@Override
-	public void deviceAdded(GenericDevice arg0) {
-		// do nothing
-	}
-
-	@Override
-	public void devicePropertyAdded(GenericDevice arg0, String arg1) {
-		// do nothing
-	}
-
-	@Override
-	public void devicePropertyModified(GenericDevice device,
-			String propertyName, Object oldValue, Object newValue) {
-
-		if (device instanceof CarbonDioxydeSensor) {
-
-			CarbonDioxydeSensor activCO2Sensor = (CarbonDioxydeSensor) device;
-			if (activCO2Sensor != null
-					&& CarbonDioxydeSensor.CARBON_DIOXYDE_SENSOR_CURRENT_CONCENTRATION
-					.equals(propertyName)) {
-
-				Double co2Contration;
-
-				if (activCO2Sensor
-						.getPropertyValue(CarbonDioxydeSensor.CARBON_DIOXYDE_SENSOR_CURRENT_CONCENTRATION) != null) {
-					// Get the current temp
-					co2Contration = (Double) activCO2Sensor
-							.getPropertyValue(CarbonDioxydeSensor.CARBON_DIOXYDE_SENSOR_CURRENT_CONCENTRATION);
-				} else
-					co2Contration = 0.0;
-
-				// Test if the temp is too High
-				if (co2Contration > SEUIL_CO2_CRITIC && co2LevelTooHigh != true) {
-
-					co2LevelTooHigh = true;
-				} else {
-					if (co2LevelTooHigh != false) {
-						co2LevelTooHigh = false;
-					}
-				}
-			}
-		}
-	}
-
-	@Override
-	public void devicePropertyRemoved(GenericDevice arg0, String arg1) {
-		// do nothing
-	}
-
-	@Override
-	public void deviceRemoved(GenericDevice arg0) {
-		// do nothing
-	}
-
-	/** Component Lifecycle Method */
-	public void stop() {
-		alarmThread.stopAlarm();
-		alarmThread = null;
-
-        /*
-           * It is extremely important to unregister the device listener.
-           * Otherwise, iCasa will continue to send notifications to the
-           * unpredictable and invalid component instance.
-           * This will also causes problem when the bundle is stopped as iCasa
-           * will still hold a reference on the device listener object.
-           * Consequently, it (and its bundle) won't be garbage collected
-           * causing a memory issue known as stale reference.
-           */
+    @Invalidate
+    public void stop() {
+        System.out.println(" Gas alarm component stop ... ");
         for (CarbonDioxydeSensor sensor : carbonDioxydeSensors) {
             sensor.removeListener(this);
         }
-	}
+    }
 
-	/** Component Lifecycle Method */
-	public void start() {
-		alarmThread = new AlarmThread("CO2GazAlarmThread"); 
-		alarmThread.start();
-	}
+
+    @Validate
+    public void start() {
+        System.out.println(" Gas alarm component start ... ");
+    }
+
+    @RequiresDevice(id="carbonDioxydeSensors", type="bind")
+    public void bindCarbonDioxydeSensor(CarbonDioxydeSensor carbonDioxydeSensor, Map properties) {
+        carbonDioxydeSensor.addListener(this);
+    }
+
+    @RequiresDevice(id="carbonDioxydeSensors", type="unbind")
+    public void unbindCarbonDioxydeSensor(CarbonDioxydeSensor carbonDioxydeSensor, Map properties) {
+        carbonDioxydeSensor.removeListener(this);
+    }
+
+    @Override
+    public void deviceAdded(GenericDevice arg0) {
+        // do nothing
+    }
+
+    @Override
+    public void devicePropertyAdded(GenericDevice arg0, String arg1) {
+        // do nothing
+    }
+
+    @Override
+    public void devicePropertyModified(GenericDevice device,String propertyName, Object oldValue, Object newValue) {
+
+    }
+
+    public boolean checkCo2() {
+        for(CarbonDioxydeSensor sensor : carbonDioxydeSensors){
+            if(sensor.getCO2Concentration() > SEUIL_CO2_CRITIC ){
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+
+    @Override
+    public void devicePropertyRemoved(GenericDevice arg0, String arg1) {
+        // do nothing
+    }
+
+    @Override
+    public void deviceEvent(GenericDevice device, Object data) {
+
+    }
+
+    @Override
+    public void deviceRemoved(GenericDevice arg0) {
+        // do nothing
+    }
+
+
+    @Override
+    public long getPeriod() {
+        synchronized (m_lock){
+            if (alarmRunning){
+                return 1000*60;
+            }else{
+                return 1000*60*10  ;
+            }
+        }
+    }
+
+    @Override
+    public String getGroup() {
+        return "Gas-Alarm-Thread-0";
+    }
+
+    @Override
+    public void run() {
+
+
+        if (checkCo2()){
+            if (!state){
+                for(BinaryLight light : binaryLights){
+                    light.turnOn();
+                }
+                for(DimmerLight light : dimmerLights){
+                    light.setPowerLevel(1);
+                }
+                state = true;
+            }else{
+                for(BinaryLight light : binaryLights){
+                    light.turnOff();
+                }
+                for(DimmerLight light : dimmerLights){
+                    light.setPowerLevel(0);
+                }
+                state = false;
+            }
+            if(alarmRunning = false){
+
+                synchronized(m_lock){
+                    alarmRunning =true;
+                }
+            }
+        }
+        if(alarmRunning = true){
+
+            synchronized(m_lock){
+                alarmRunning =false;
+            }
+        }
+
+    }
+
 }
+
+/*
+@Component(name = "GasAlarm")
+@Instantiate
+@Provides
+public class GasAlarmSystemApplication implements DeviceListener,PeriodicRunnable {
+
+
+
+    public static final double SEUIL_CO2_CRITIC = 3.8;
+
+    private boolean state = false;
+
+    private boolean alarmRunning = false;
+
+    private final Object m_lock;
+
+
+
+    @RequiresDevice(id="binaryLights", type="field", optional=true)
+    private BinaryLight[] binaryLights;
+
+    @RequiresDevice(id="dimmerLights", type="field", optional=true)
+    private DimmerLight[] dimmerLights;
+
+    @RequiresDevice(id="carbonDioxydeSensors", type="field", optional=true)
+    private CarbonDioxydeSensor[] carbonDioxydeSensors;
+
+    public GasAlarmSystemApplication() {
+        this.m_lock = new Object();
+    }
+
+
+    @Invalidate
+    public void stop() {
+        System.out.println(" Gas alarm component stop ... ");
+        for (CarbonDioxydeSensor sensor : carbonDioxydeSensors) {
+            sensor.removeListener(this);
+        }
+    }
+
+
+    @Validate
+    public void start() {
+        System.out.println(" Gas alarm component start ... ");
+    }
+
+    @RequiresDevice(id="carbonDioxydeSensors", type="bind")
+    public void bindCarbonDioxydeSensor(CarbonDioxydeSensor carbonDioxydeSensor, Map properties) {
+        carbonDioxydeSensor.addListener(this);
+    }
+
+    @RequiresDevice(id="carbonDioxydeSensors", type="unbind")
+    public void unbindCarbonDioxydeSensor(CarbonDioxydeSensor carbonDioxydeSensor, Map properties) {
+        carbonDioxydeSensor.removeListener(this);
+    }
+
+    @Override
+    public void deviceAdded(GenericDevice arg0) {
+        // do nothing
+    }
+
+    @Override
+    public void devicePropertyAdded(GenericDevice arg0, String arg1) {
+        // do nothing
+    }
+
+    @Override
+    public void devicePropertyModified(GenericDevice device,String propertyName, Object oldValue, Object newValue) {
+
+    }
+
+    public boolean checkCo2() {
+        for(CarbonDioxydeSensor sensor : carbonDioxydeSensors){
+            if(sensor.getCO2Concentration() > SEUIL_CO2_CRITIC ){
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+
+    @Override
+    public void devicePropertyRemoved(GenericDevice arg0, String arg1) {
+        // do nothing
+    }
+
+    @Override
+    public void deviceEvent(GenericDevice device, Object data) {
+
+    }
+
+    @Override
+    public void deviceRemoved(GenericDevice arg0) {
+        // do nothing
+    }
+
+
+    @Override
+    public long getPeriod() {
+        synchronized (m_lock){
+            if (alarmRunning){
+                return 1000*60;
+            }else{
+                return 1000*60*10  ;
+            }
+        }
+    }
+
+    @Override
+    public String getGroup() {
+        return "Gas-Alarm-Thread-0";
+    }
+
+    @Override
+    public void run() {
+
+
+        if (checkCo2()){
+            if (!state){
+                for(BinaryLight light : binaryLights){
+                    light.turnOn();
+                }
+                for(DimmerLight light : dimmerLights){
+                    light.setPowerLevel(1);
+                }
+                state = true;
+            }else{
+                for(BinaryLight light : binaryLights){
+                    light.turnOff();
+                }
+                for(DimmerLight light : dimmerLights){
+                    light.setPowerLevel(0);
+                }
+                state = false;
+            }
+            if(alarmRunning = false){
+
+                synchronized(m_lock){
+                    alarmRunning =true;
+                }
+            }
+        }
+        if(alarmRunning = true){
+
+            synchronized(m_lock){
+                alarmRunning =false;
+            }
+        }
+
+    }
+}
+*/
