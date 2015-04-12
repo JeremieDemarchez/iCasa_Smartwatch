0da52bdb4e3c859376df04832172f5517d888260
aygalinc <colin.aygalinc@gmail.com>
2015-04-12 21:00:13 +0200

    Mode is a wisdom Controller in order to be set by the Web UI

--- applications/homeLive/application/mode/src/main/java/fr/liglab/adele/icasa/mode/ModeServiceImpl.java
+++ applications/homeLive/application/mode/src/main/java/fr/liglab/adele/icasa/mode/ModeServiceImpl.java
@@ -3,11 +3,16 @@ package fr.liglab.adele.icasa.mode;
 import org.apache.felix.ipojo.annotations.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.wisdom.api.DefaultController;
+import org.wisdom.api.annotations.FormParameter;
+import org.wisdom.api.annotations.Route;
+import org.wisdom.api.http.HttpMethod;
+import org.wisdom.api.http.Result;
 
 @Component
 @Instantiate
 @Provides
-public class ModeServiceImpl implements ModeService {
+public class ModeServiceImpl extends DefaultController implements ModeService {
 
     private  final Logger m_logger = LoggerFactory
             .getLogger(ModeServiceImpl.class);
@@ -53,4 +58,11 @@ public class ModeServiceImpl implements ModeService {
             }
         }
     }
+
+    @Route(method = HttpMethod.POST,uri = "/homelive/mode")
+    public Result setMode(@FormParameter("mode") String modeName){
+        setCurrentMode(modeName);
+        return ok(getCurrentMode()).json();
+    }
+
 }
