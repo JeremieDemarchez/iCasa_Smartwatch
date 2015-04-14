0efd95c00cf5c656325a17a7636b81cda5c71b41
aygalinc <colin.aygalinc@gmail.com>
2015-04-14 18:44:20 +0200

    Implem Command Handler

--- applications/homeLive/application/homeLiveConfigurator/src/main/java/fr/liglab/adele/icasa/home/live/configurator/HomeLiveApplicationConfiguration.java
+++ applications/homeLive/application/homeLiveConfigurator/src/main/java/fr/liglab/adele/icasa/home/live/configurator/HomeLiveApplicationConfiguration.java
@@ -1,8 +1,15 @@
 package fr.liglab.adele.icasa.home.live.configurator;
 
 import fr.liglab.adele.icasa.access.AccessManager;
+import fr.liglab.adele.icasa.access.AccessRight;
 import fr.liglab.adele.icasa.access.DeviceAccessPolicy;
+import fr.liglab.adele.icasa.mode.ModeUtils;
 import org.slf4j.Logger;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * Created by aygalinc on 13/04/15.
@@ -13,30 +20,98 @@ public class HomeLiveApplicationConfiguration {
 
     private final AccessManager accessManager;
 
-    private final Logger m_logger   ;
-    public HomeLiveApplicationConfiguration(String appliId, AccessManager accessManage,Logger logger){
+    private final Logger m_logger ;
+
+    private  String m_currentMode ;
+
+    private final Map<String,Map<String,String>> m_permissionByMode =new HashMap<>()  ;
+
+    public HomeLiveApplicationConfiguration(String appliId, AccessManager accessManage,Logger logger,String currentMode){
         m_applicationId = appliId;
         this.accessManager = accessManage;
         m_logger =logger;
+        m_currentMode = currentMode;
+
+        Map<String,String> permission = new HashMap<String,String>();
+        AccessRight[] appliRights = accessManager.getAccessRight(this.m_applicationId);
+        for (AccessRight accessRight : appliRights){
+            permission.put(accessRight.getDeviceId(), accessRight.getPolicy().toString());
+        }
+        m_permissionByMode.put(ModeUtils.AWAY,new HashMap<>(permission));
+
+        m_permissionByMode.put(ModeUtils.HOLIDAYS,new HashMap<>(permission));
+
+        m_permissionByMode.put(ModeUtils.HOME,new HashMap<>(permission));
+
+        m_permissionByMode.put(ModeUtils.NIGHT,new HashMap<>(permission));
     }
 
-    public void addDeviceWithPermission(String deviceId,boolean permission){
-      /*  try {
-            jsonAccessRight = AccessRightJSON.fromString(content);
-            if (permission){
-                right = manager.setDeviceAccess(m_applicationId,deviceId, DeviceAccessPolicy.fromString(jsonAccessRight.getString("policy")));
+    public synchronized Map<String,String> getDeviceWithPermissions(){
+        Map<String,String> deviceUsed = new HashMap<>();
+        AccessRight[] appliRights = accessManager.getAccessRight(this.m_applicationId);
+        for (AccessRight accessRight : appliRights){
+            deviceUsed.put(accessRight.getDeviceId(), accessRight.getPolicy().toString());
+        }
+        return deviceUsed;
+    }
+
+
+    private synchronized AccessRight updatePermission(String deviceId,String permission) {
+        AccessRight[] appliRights = accessManager.getAccessRight(this.m_applicationId);
+        for (AccessRight appliRight : appliRights) {
+            if(appliRight.getDeviceId().equals(deviceId)){
+                try {
+                    AccessRight right = null;
+                    if (permission.equals("total") ||permission.equals("hidden") ){
+                        right = accessManager.setDeviceAccess(m_applicationId, deviceId,DeviceAccessPolicy.fromString(permission));
+                    }
+                    return right;
+                } catch (Exception e) {
+                    m_logger.error("Update permission Fail ! ");
+                }
             }
-        } catch (Exception e) {
-
-        }*/
+        }
+        return null;
     }
 
+    public synchronized Map<String,String> updatePermission(String deviceId,String permission,String Mode) {
+        Map<String,String> returnMap = new HashMap<String,String>();
+        if (m_currentMode.equals(Mode)) {
+            AccessRight right = updatePermission(deviceId,permission);
+            returnMap.put(right.getDeviceId(),right.getPolicy().toString());
+            Map<String, String> permissionByDevice = new HashMap<String, String>();
+            AccessRight[] appliRights = accessManager.getAccessRight(this.m_applicationId);
+            for (AccessRight accessRight : appliRights) {
+                permissionByDevice.put(accessRight.getDeviceId(), accessRight.getPolicy().toString());
+            }
+            m_permissionByMode.put(Mode, permissionByDevice);
+            return returnMap;
+        }
+        if (Mode.equals(ModeUtils.AWAY) || Mode.equals(ModeUtils.NIGHT) ||Mode.equals(ModeUtils.HOLIDAYS) ||Mode.equals(ModeUtils.HOME) ){
 
-    public void removeDevice(String deviceId){
-
+            if (accessManager.getAccessRight(m_applicationId,deviceId) !=null ){
+                if(permission.equals("hidden") || permission.equals("total")){
+                    m_permissionByMode.get(m_currentMode).put(deviceId,permission);
+                    returnMap.put(deviceId,permission);
+                    return returnMap;
+                }else{
+                    m_logger.error(" POLICY INVALID" + permission);
+                }
+            }else {
+                m_logger.error(" DEVICE ID INVALID" +deviceId);
+            }
+        }
+        m_logger.error(" MODE INVALID IN UPDATE PERMISSION" + Mode);
+        return returnMap;
     }
 
-    public void updatePermission(){
-
+    public synchronized void changeCurrentMode(String mode) {
+        if (mode.equals(ModeUtils.AWAY) || mode.equals(ModeUtils.NIGHT) ||mode.equals(ModeUtils.HOLIDAYS) ||mode.equals(ModeUtils.HOME) ){
+            m_currentMode = mode;
+            for (String deviceId : m_permissionByMode.get(mode).keySet()){
+                updatePermission(deviceId,m_permissionByMode.get(mode).get(deviceId));
+            }
+        }
+        m_logger.error(" INVALID MODE NAME IN CHANGE MODE " + mode);
     }
 }
--- applications/homeLive/application/homeLiveConfigurator/src/main/java/fr/liglab/adele/icasa/home/live/configurator/HomeLiveConfigurator.java
+++ applications/homeLive/application/homeLiveConfigurator/src/main/java/fr/liglab/adele/icasa/home/live/configurator/HomeLiveConfigurator.java
@@ -1,26 +1,53 @@
 package fr.liglab.adele.icasa.home.live.configurator;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import fr.liglab.adele.icasa.access.AccessManager;
+import fr.liglab.adele.icasa.access.AccessRight;
+import fr.liglab.adele.icasa.access.AccessRightManagerListener;
 import fr.liglab.adele.icasa.alarm.AlarmService;
+import fr.liglab.adele.icasa.application.Application;
+import fr.liglab.adele.icasa.application.ApplicationManager;
+import fr.liglab.adele.icasa.application.ApplicationTracker;
 import fr.liglab.adele.icasa.mode.ModeService;
 import fr.liglab.adele.icasa.mode.ModeServiceImpl;
 import fr.liglab.adele.icasa.notification.NotificationService;
 import org.apache.felix.ipojo.annotations.*;
+import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.wisdom.api.DefaultController;
 import org.wisdom.api.annotations.FormParameter;
+import org.wisdom.api.annotations.Opened;
+import org.wisdom.api.annotations.Parameter;
 import org.wisdom.api.annotations.Route;
+import org.wisdom.api.content.Json;
 import org.wisdom.api.http.HttpMethod;
 import org.wisdom.api.http.Result;
+import org.wisdom.api.http.websockets.Publisher;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Component
 @Instantiate
 @Provides
-public class HomeLiveConfigurator extends DefaultController {
+public class HomeLiveConfigurator extends DefaultController implements ApplicationTracker,AccessRightManagerListener {
+
+    public final static String HOMELIVE_WEB_SOCKET = "/homelive/ws";
 
     private  final Logger m_logger = LoggerFactory
             .getLogger(ModeServiceImpl.class);
+
+    private final Map<String,HomeLiveApplicationConfiguration> homeLiveConfigurationAppMap = new HashMap<>();
+
+    private final Object m_lock = new Object();
+
+    @Requires
+    Json json;
+
+    @Requires
+    Publisher publisher;
 
     @Requires
     ModeService modeService;
@@ -34,6 +61,9 @@ public class HomeLiveConfigurator extends DefaultController {
     @Requires
     AccessManager accessManager;
 
+    @Requires
+    ApplicationManager applicationManager;
+
     public HomeLiveConfigurator(){
 
     }
@@ -41,11 +71,43 @@ public class HomeLiveConfigurator extends DefaultController {
     @Validate
     public void start(){
         m_logger.info("HOME LIVE CONFIGURATOR Service STARTING");
+        applicationManager.addApplicationListener(this);
+        accessManager.addListener(this);
     }
 
     @Invalidate
     public void stop(){
         m_logger.info("HOME LIVE CONFIGURATOR Service STOPPING");
+        accessManager.removeListener(this);
+        applicationManager.removeApplicationListener(this);
+    }
+
+    @Opened("/homelive/ws")
+    public void opened(@Parameter("client") String client) {
+        synchronized (m_lock){
+            for (String id : homeLiveConfigurationAppMap.keySet()){
+                HomeLiveApplicationConfiguration appliConfig = homeLiveConfigurationAppMap.get(id);
+                Map<String,String> devicePermissions = appliConfig.getDeviceWithPermissions();
+                for (String deviceId : devicePermissions.keySet()){
+                    publisher.send(HOMELIVE_WEB_SOCKET, client,(new SendPackage(id,deviceId,devicePermissions.get(deviceId),modeService.getCurrentMode())).toJson());
+                }
+            }
+        }
+
+    }
+
+    @Route(method = HttpMethod.POST,uri = "/homelive/permission")
+    public Result setPermission(@FormParameter("appliId") String appliId,@FormParameter("deviceId") String deviceId,@FormParameter("permission") String permission,@FormParameter("mode") String mode){
+        if (homeLiveConfigurationAppMap.containsKey(appliId)){
+            Map<String,String> returnMap =  homeLiveConfigurationAppMap.get(appliId).updatePermission(deviceId,permission,mode);
+            return ok();
+        }
+        return badRequest();
+    }
+
+    @Route(method = HttpMethod.GET,uri = "/homelive/mode")
+    public Result getMode(){
+        return ok(modeService.getCurrentMode()).json();
     }
 
     @Route(method = HttpMethod.POST,uri = "/homelive/mode")
@@ -54,10 +116,20 @@ public class HomeLiveConfigurator extends DefaultController {
         return ok(modeService.getCurrentMode()).json();
     }
 
+    @Route(method = HttpMethod.GET,uri = "/homelive/notification")
+    public Result getUserAddress(){
+        return ok(notificationService.getUserAddress()).json();
+    }
+
     @Route(method = HttpMethod.POST,uri = "/homelive/notification")
     public Result setUserAddress(@FormParameter("mode") String userAdress){
         notificationService.setUserAddress(userAdress);
         return ok(notificationService.getUserAddress()).json();
+    }
+
+    @Route(method = HttpMethod.GET,uri = "/homelive/alarm/camera")
+    public Result getAlarmCamera(){
+        return ok(alarmService.getAlarmCameraStatus()).json();
     }
 
     @Route(method = HttpMethod.POST,uri = "/homelive/alarm/camera")
@@ -66,10 +138,133 @@ public class HomeLiveConfigurator extends DefaultController {
         return ok(alarmService.getAlarmCameraStatus()).json();
     }
 
+    @Route(method = HttpMethod.GET,uri = "/homelive/alarm/sound")
+    public Result getAlarmSound(){
+        return ok(alarmService.getAlarmSoundStatus()).json();
+    }
+
     @Route(method = HttpMethod.POST,uri = "/homelive/alarm/sound")
     public Result setAlarmSound(@FormParameter("mode") Boolean soundStatus){
         alarmService.setAlarmSoundStatus(soundStatus);
         return ok(alarmService.getAlarmSoundStatus()).json();
     }
 
+    @Override
+    public void addApplication(Application app) {
+
+    }
+
+    @Override
+    public void removeApplication(Application app) {
+
+    }
+
+    @Override
+    public void deploymentPackageAdded(Application app, String symbolicName) {
+
+
+    }
+
+    @Override
+    public void deploymentPackageRemoved(Application app, String symbolicName) {
+
+    }
+
+    @Override
+    public void bundleAdded(Application app, String symbolicName) {
+        synchronized (m_lock){
+            if (homeLiveConfigurationAppMap.containsKey(app.getId())){
+                //APP DEJA ENREGISTRE
+            }else {
+                homeLiveConfigurationAppMap.put(app.getId(),new HomeLiveApplicationConfiguration(app.getId(),accessManager,m_logger,modeService.getCurrentMode()));
+                HomeLiveApplicationConfiguration appliConfig = homeLiveConfigurationAppMap.get(app.getId());
+                Map<String,String> devicePermissions = appliConfig.getDeviceWithPermissions();
+                for (String deviceId : devicePermissions.keySet()){
+                    publisher.publish(HOMELIVE_WEB_SOCKET, (new SendPackage(app.getId(), deviceId, devicePermissions.get(deviceId),modeService.getCurrentMode())).toJson());
+                }
+            }
+        }
+    }
+
+    @Override
+    public void bundleRemoved(Application app, String symbolicName) {
+        synchronized (m_lock){
+            if (homeLiveConfigurationAppMap.containsKey(app.getId())){
+                //APP DEJA ENREGISTRE
+                homeLiveConfigurationAppMap.remove(app.getId());
+            }
+        }
+    }
+
+    @Override
+    public void onAccessRightAdded(AccessRight accessRight) {
+        String appId = accessRight.getApplicationId();
+        synchronized (m_lock){
+            if (homeLiveConfigurationAppMap.containsKey(appId)){
+                String policy = accessRight.getPolicy().toString();
+                String deviceId = accessRight.getDeviceId();
+                publisher.publish(HOMELIVE_WEB_SOCKET,(new SendPackage(appId,deviceId,policy,modeService.getCurrentMode())).toJson());
+            }else {
+                homeLiveConfigurationAppMap.put(appId, new HomeLiveApplicationConfiguration(appId, accessManager, m_logger,modeService.getCurrentMode()));
+                HomeLiveApplicationConfiguration appliConfig = homeLiveConfigurationAppMap.get(appId);
+                Map<String,String> devicePermissions = appliConfig.getDeviceWithPermissions();
+                for (String deviceId : devicePermissions.keySet()){
+                    publisher.publish(HOMELIVE_WEB_SOCKET, (new SendPackage(appId, deviceId, devicePermissions.get(deviceId),modeService.getCurrentMode())).toJson());
+                }
+            }
+        }
+
+    }
+
+    @Override
+    public void onAccessRightModified(AccessRight accessRight) {
+        String appId = accessRight.getApplicationId();
+        synchronized (m_lock){
+            if (homeLiveConfigurationAppMap.containsKey(appId)){
+                String policy = accessRight.getPolicy().toString();
+                String deviceId = accessRight.getDeviceId();
+                publisher.publish(HOMELIVE_WEB_SOCKET,(new SendPackage(appId,deviceId,policy,modeService.getCurrentMode())).toJson());
+            }else {
+                homeLiveConfigurationAppMap.put(appId, new HomeLiveApplicationConfiguration(appId, accessManager, m_logger,modeService.getCurrentMode()));
+                HomeLiveApplicationConfiguration appliConfig = homeLiveConfigurationAppMap.get(appId);
+                Map<String,String> devicePermissions = appliConfig.getDeviceWithPermissions();
+                for (String deviceId : devicePermissions.keySet()){
+                    publisher.publish(HOMELIVE_WEB_SOCKET, (new SendPackage(appId, deviceId, devicePermissions.get(deviceId),modeService.getCurrentMode())).toJson());
+                }
+            }
+        }
+    }
+
+    @Override
+    public void onMethodAccessRightModified(AccessRight accessRight, String methodName) {
+
+    }
+
+
+    private class SendPackage{
+
+        private final String m_appId;
+
+        private final String m_deviceId;
+
+        private final String m_permission;
+
+        private final String m_mode;
+
+        private SendPackage(String appId,String deviceId,String permission,String mode){
+            m_appId = appId;
+            m_deviceId = deviceId;
+            m_permission = permission;
+            m_mode = mode;
+        }
+
+        private JsonNode toJson(){
+            ObjectNode result = json.newObject();
+            result.put("appId", m_appId);
+            result.put("deviceId", m_deviceId);
+            result.put("permission", m_permission);
+            result.put("mode", m_mode);
+            return result;
+        }
+    }
 }
