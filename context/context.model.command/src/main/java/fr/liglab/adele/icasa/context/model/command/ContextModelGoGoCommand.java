90139cd7d2bd1db4c225e2b84dcbbf08f1ba9b58
aygalinc <colin.aygalinc@gmail.com>
2015-09-17 17:03:16 +0200

    Fix gogo command

--- context/context.model.command/src/main/java/fr/liglab/adele/icasa/context/model/command/ContextModelGoGoCommand.java
+++ context/context.model.command/src/main/java/fr/liglab/adele/icasa/context/model/command/ContextModelGoGoCommand.java
@@ -14,20 +14,53 @@ import java.util.List;
 
 @Component
 @Instantiate
-@CommandProvider(namespace = "HomeLive")
+@CommandProvider(namespace = "context")
 public class ContextModelGoGoCommand  {
 
     private static final Logger LOG = LoggerFactory.getLogger(ContextModelGoGoCommand.class);
 
-    @Requires(specification = Relation.class)
+    @Requires(specification = Relation.class,optional = true,proxy = false)
     List<Relation> relations;
 
     @Command
     public void relations(){
         for (Relation relation: relations){
+            LOG.info("Relation Name : " + relation.getName());
+            LOG.info(relation.getSource() + " --------> " + relation.getEnd());
+            System.out.println("\n");
+        }
+    }
 
-        LOG.info("Relation Name : " + relation.getName());
-            LOG.info(relation.getEnd() + " --------> " + relation.getEnd());
+    @Command
+    public void relationsByName(String name){
+        for (Relation relation: relations) {
+            if (relation.getName().equals(name)) {
+                LOG.info("Relation Name : " + relation.getName());
+                LOG.info(relation.getSource() + " --------> " + relation.getEnd());
+                System.out.println("\n");
+            }
+        }
+    }
+
+    @Command
+    public void relationsBySource(String source){
+        for (Relation relation: relations) {
+            if (relation.getSource().equals(source)) {
+                LOG.info("Relation Name : " + relation.getName());
+                LOG.info(relation.getSource() + " --------> " + relation.getEnd());
+                System.out.println("\n");
+            }
+        }
+    }
+
+    @Command
+    public void relationsByEnd(String end){
+        for (Relation relation: relations) {
+            if (relation.getEnd().equals(end)) {
+                LOG.info("Relation Name : " + relation.getName());
+                LOG.info(relation.getSource() + " --------> " + relation.getEnd());
+                System.out.println("\n");
+            }
         }
     }
 
