f1852da45133bb151ac95083c5ce4aef294219e0
aygalinc <colin.aygalinc@gmail.com>
2015-10-08 16:31:07 +0200

    Add target to ContextEntity Annotation

--- context/context.model/src/main/java/fr/liglab/adele/icasa/context/handler/ContextEntity.java
+++ context/context.model/src/main/java/fr/liglab/adele/icasa/context/handler/ContextEntity.java
@@ -1,8 +1,12 @@
 package fr.liglab.adele.icasa.context.handler;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
 /**
  * Created by aygalinc on 06/10/15.
  */
+@Target(ElementType.TYPE)
 public @interface ContextEntity {
 
 
