025cd3d5213083f3ac2c9737e36e5fd83d37526f
aygalinc <colin.aygalinc@gmail.com>
2015-10-09 15:34:58 +0200

    Optimize import

--- context/context.model.example/src/main/java/fr/liglab/adele/icasa/context/model/example/application/LightFollowRegulatorImpl.java
+++ context/context.model.example/src/main/java/fr/liglab/adele/icasa/context/model/example/application/LightFollowRegulatorImpl.java
@@ -8,10 +8,7 @@ import org.apache.felix.ipojo.annotations.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @Component(immediate = true)
 @Provides
