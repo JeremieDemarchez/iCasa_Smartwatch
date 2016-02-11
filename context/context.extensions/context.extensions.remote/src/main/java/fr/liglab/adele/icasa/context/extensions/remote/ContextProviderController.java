874435eacfd1f5255380ca9ead1beeab161b8d63
aygalinc <colin.aygalinc@gmail.com>
2016-02-11 13:51:07 +0100

    Fix contextProviderController with new Creator Introspection Class

--- context/context.extensions/context.extensions.remote/src/main/java/fr/liglab/adele/icasa/context/extensions/remote/ContextProviderController.java
+++ context/context.extensions/context.extensions.remote/src/main/java/fr/liglab/adele/icasa/context/extensions/remote/ContextProviderController.java
@@ -30,7 +30,6 @@ import org.wisdom.api.content.Json;
 import org.wisdom.api.http.HttpMethod;
 import org.wisdom.api.http.Result;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -39,91 +38,91 @@ public class ContextProviderController extends DefaultController {
     @Requires
     Json json;
 
-    @Requires(specification = EntityProvider.class,optional = true)
+    @Requires(specification = EntityProvider.class, optional = true)
     List<EntityProvider> creatorHandlers;
 
-//    List<EntityCreatorHandlerIntrospection> fake = new ArrayList<EntityCreatorHandlerIntrospection>();
-//
-//    ContextProviderController(){
-//        fake.add(0,new FakeCreator("Creator1","fr.entity.1"));
-//        fake.add(1,new FakeCreator("Creator2","fr.entity.2"));
-//        fake.add(1,new FakeCreator("German","fr.entity.sd2"));
-//    }
-
     @Route(method = HttpMethod.GET, uri = "/context/providers")
-    public Result getContextProviders(){
+    public Result getContextProviders() {
         ObjectNode result = json.newObject();
 
 
-        for (EntityProvider creatorHandler : creatorHandlers){
-            result.put(creatorHandler.getName(),true);
+        for (EntityProvider creatorHandler : creatorHandlers) {
+            result.put(creatorHandler.getName(), true);
         }
 
         return ok(result);
     }
 
     @Route(method = HttpMethod.GET, uri = "/context/providers/{id}")
-    public Result getContextProvider(@Parameter(value = "id") String id){
-        if (id == null){
+    public Result getContextProvider(@Parameter(value = "id") String id) {
+        if (id == null) {
             return internalServerError(new NullPointerException("Provider id is null"));
         }
 
         ObjectNode result = json.newObject();
 
-        for (EntityProvider creatorHandler : creatorHandlers){
-            if (creatorHandler.getName().equals(id)){
-                for (String implemSpecification : creatorHandler.getProvidedEntities()){
-                    result.put(implemSpecification,creatorHandler.isEnabled(implemSpecification));
-                }
-                return ok(result);
+        for (EntityProvider creatorHandler : creatorHandlers) {
+            if (! creatorHandler.getName().equals(id)) {
+                continue;
             }
+            for (String implemSpecification : creatorHandler.getProvidedEntities()) {
+                result.put(implemSpecification, creatorHandler.isEnabled(implemSpecification));
+            }
+            return ok(result);
         }
         return notFound();
     }
 
     @Route(method = HttpMethod.GET, uri = "/context/providers/{id}/{implem}")
-    public Result getContextProviderImpl(@Parameter(value = "id") String id, @Parameter(value = "implem") String implem){
-        if (id == null){
+    public Result getContextProviderImpl(@Parameter(value = "id") String id, @Parameter(value = "implem") String implem) {
+        if (id == null) {
             return internalServerError(new NullPointerException("Provider id is null"));
         }
 
         ObjectNode result = json.newObject();
 
-        for (EntityCreatorHandlerIntrospection creatorHandler : creatorHandlers) {
-            if (creatorHandler.getAttachedComponentInstanceName().equals(id)) {
-                for (String implemSpecification : creatorHandler.getImplementations()) {
-                    if (implemSpecification.equals(implem)) {
-                        result.put(implemSpecification, creatorHandler.getImplentationState(implemSpecification));
-                    }
-                }
-                return ok(result);
+        for (EntityProvider creatorHandler : creatorHandlers) {
+            if (! creatorHandler.getName().equals(id)) {
+                continue;
             }
+            for (String implemSpecification : creatorHandler.getProvidedEntities()) {
+                if (implemSpecification.equals(implem)) {
+                    result.put(implemSpecification, creatorHandler.isEnabled(implemSpecification));
+                }
+            }
+            return ok(result);
         }
+
 
         return notFound();
     }
 
     @Route(method = HttpMethod.POST, uri = "/context/providers/{id}/{implem}/{state}")
-    public Result switchContextProviderState(@Parameter(value = "id") String id, @Parameter(value = "implem") String implem, @Parameter(value = "state") boolean state){
-        if (id == null){
+    public Result switchContextProviderState(@Parameter(value = "id") String id, @Parameter(value = "implem") String implem, @Parameter(value = "state") boolean state) {
+        if (id == null) {
             return internalServerError(new NullPointerException("Provider id is null"));
         }
 
         ObjectNode result = json.newObject();
 
 
-        for (EntityCreatorHandlerIntrospection creatorHandler : creatorHandlers) {
-            if (creatorHandler.getAttachedComponentInstanceName().equals(id)) {
-                for (String implemSpecification : creatorHandler.getImplementations()) {
-                    if (implemSpecification.equals(implem)) {
-                        creatorHandler.switchCreation(implem, state);
-                        result.put(implemSpecification, creatorHandler.getImplentationState(implemSpecification));
+        for (EntityProvider creatorHandler : creatorHandlers) {
+            if (!creatorHandler.getName().equals(id)) {
+                continue;
+            }
+            for (String implemSpecification : creatorHandler.getProvidedEntities()) {
+                if (implemSpecification.equals(implem)) {
+                    if (state){
+                        creatorHandler.enable(implem);
                     }
+                    else {
+                        creatorHandler.disable(implem);
+                    }
+                    result.put(implemSpecification, creatorHandler.isEnabled(implemSpecification));
+                    return ok(result);
                 }
-                return ok(result);
             }
         }
-
         return notFound();
     }
 }
